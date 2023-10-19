24abe38631649ba83d333b5afbecddfed4bf19dc
Dante Yupanqui <danteyupanqui2811@gmail.com>
2023-10-19 11:46:32 -0400

    primer componente

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { useState } from 'react'
+import{primer_componente} from './assets/primer_componente'
+
+import './App.css'
+
+function App() {
+  return (
+  <div className='App-container'>
+    {primer_componente.map(car=>{
+      
+    })
+      
+    }
+  </div>
+  )
+}
+
+export default App
---
+++ src/primer_componente.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function primer_componente(props) {
+  const primer_componente=props
+  const theme=primer_componente.theme
+  return(
+    
+    <section className={`componente1${theme}`}>
+
+
+      
+    </section>
+
+
+  )
+}
+export default primer_componente
