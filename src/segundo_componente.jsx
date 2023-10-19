914ac229d1fb081052cbe3fb932a063267fc0c87
Dante Yupanqui <danteyupanqui2811@gmail.com>
2023-10-19 11:52:33 -0400

    segundo componente

---
+++ src/segundo_componente.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function segundo_componente(props) {
+  const segundo_componente=props
+  const theme=segundo_componente.theme
+  return(
+    
+    <section className={`componente2${theme}`}>
+
+
+      
+    </section>
+
+
+  )
+}
+export default segundo_componente
